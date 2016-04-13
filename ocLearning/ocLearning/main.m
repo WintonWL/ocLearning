dc9a6405caaeeb2954a91ac2a0489fdcec4810aa
翁乐 <wengle@outlook.com>
2016-04-13 22:25:39 +0800

    file的读取

---
+++ ocLearning/ocLearning/main.m
@@ -0,0 +1,41 @@
+//
+//  main.m
+//  ocLearning
+//
+//  Created by 翁乐 on 4/13/16.
+//  Copyright © 2016 翁乐. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+FILE *fileReader(const char * filename)
+{
+    FILE *wordFile = fopen(filename, "r");
+    char word[100];
+    
+    
+    while (fgets(word,100,wordFile)) {
+        word[strlen(word) - 1] = '\0';
+        
+        NSLog(@"%s is %lu characters long",word,strlen(word));
+    }
+    
+    fclose(wordFile);
+    
+    return wordFile;
+}
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        if (argc == 1) {
+            NSLog(@"you need to provide a file name");
+            return 1;
+        }
+        
+        fileReader(argv[1]);
+       
+    }
+    return 0;
+}
